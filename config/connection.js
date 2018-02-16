e4484f5c4aaeaecdb84bf3060fab4069b80d6139
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-02-16 09:30:03 -0800

    jawsDB changes

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 //setting up MySQL connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "burgers_db"
-});
+if (process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: "",
+		database: "burgers_db"
+	});
+};
 
-//Make the connection
-connection.connect(function(err){
-	if(err) throw err
-	console.log("connected as id: " + connection.threadId);
-});
-
+connection.connect();
 //Export the established connection for ORM.js to use
 module.exports = connection;
